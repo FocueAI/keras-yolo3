99740925667a3f6caea833daf3157354b2516f40
侯胜超 <houshengchao@findpiano.cn>
2022-06-03 22:32:18 +0800

    重命名数据增强的部分文件名字

--- yolov3_data_enhance/step1-choice_testdatasets.py
+++ yolov3_data_enhance/step1-choice_testdatasets.py
@@ -2,26 +2,28 @@ import os, shutil
 from glob import glob
 import random
 
-choice_testsets_path = r'F:\各种公司\jst2\第2批数据\all_img\testdataset'
+choice_testsets_path = r'F:\各种公司\jst2\第3批数据\fix_data\fix_data\testdataset'
 os.makedirs(choice_testsets_path,exist_ok=True)
 
-img_path1 = r'F:\各种公司\jst2\第2批数据\all_img\all_img\*.png'
-img_path2 = r'F:\各种公司\jst2\第2批数据\all_img\all_img\*.jpg'
+# img_path1 = r'F:\各种公司\jst2\第2批数据\all_img\all_img\*.png'
+img_path2 = r'F:\各种公司\jst2\第3批数据\fix_data\fix_data\img\*.jpg'
+label_path = r'F:\各种公司\jst2\第3批数据\fix_data\fix_data\label'
 
-
-img_list = glob(img_path1) + glob(img_path2)
+img_list = glob(img_path2) #+ glob(img_path2)
 random.shuffle(img_list)
+print(len(img_list))
 
 testdatasets_ratio = 0.1 # 挑选的测试集 占 总样本数量的 比例
 
 for i in img_list[:int(0.1 * len(img_list))]:
-    file_name, file_extend_name = os.path.splitext(i)
+    dir_path, full_file_name = os.path.split(i)
+    file_name, extend_name = os.path.splitext(full_file_name)
     # print(f'file_name:{file_name}')
     # print(f'file_extend_name:{file_extend_name}')
     src_img_path = i
-    src_xml_path = file_name + '.xml'
+    src_xml_path = os.path.join(label_path,file_name + '.xml')
 
-    print(f'{os.path.split(i)}')
+    # print(f'{os.path.split(i)}')
     dst_img_path = os.path.join(choice_testsets_path,os.path.basename(src_img_path))
     dst_label_path = os.path.join(choice_testsets_path, os.path.basename(src_xml_path))
     #
